53c01985addcf782ff70139b5edbbd2908662d4e
mbtitoolbox <mbtitoolbox@gmail.com>
2025-01-04 21:14:45 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,34 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import LineBotApiError, InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello, World!"
+# 從環境變數獲取 Line Bot Secret 和 Access Token
+line_bot_api = LineBotApi(os.getenv('LINE_CHANNEL_ACCESS_TOKEN'))
+handler = WebhookHandler(os.getenv('LINE_CHANNEL_SECRET'))
 
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 8000))
-    app.run(host="0.0.0.0", port=port)
+@app.route("/callback", methods=['POST'])
+def callback():
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextMessage(text="Hello, world!")
+    )
+
+if __name__ == "__main__":
+    app.run()
+
