b1abc545b63a54b9f4c8cf96b32e434ae01a9c18
mbtitoolbox <mbtitoolbox@gmail.com>
2025-01-05 15:47:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,8 @@ from mbti import handle_mbtimessages  # 引入處理 MBTI 訊息的函數
 app = Flask(__name__)
 
 # LINE BOT 配置
-line_bot_api = LineBotApi('LINE_CHANNEL_ACCESS_TOKEN')  # 替換為您的 Channel Access Token
-handler = WebhookHandler('LINE_CHANNEL_SECRET')  # 替換為您的 Channel Secret
+line_bot_api = LineBotApi('ixIjKiibYZdUn4W9ZZAPS5lgAt4JAsxW/nLrnmJWfCu5Vh19nerq/nooyzzsDL0SMr/DwBq+vGhKPWA+p/yzPINc9DvoRJ4f1qWxY2eb+ujWfFPbqx+6Ra0/Jbjh0zg18fqC/Mlak61+EXFkcUECgQdB04t89/1O/w1cDnyilFU=')  # 替換為您的 Channel Access Token
+handler = WebhookHandler('3e49258295882026968a5788967a12f1')  # 替換為您的 Channel Secret
 
 @app.route("/callback", methods=['POST'])
 def callback():
