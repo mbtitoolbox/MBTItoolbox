aa336f3a526c2b1ae4158cb270a4aed311f50806
mbtitoolbox <mbtitoolbox@gmail.com>
2025-01-05 17:41:05 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ app = Flask(__name__)
 line_bot_api = LineBotApi('ixIjKiibYZdUn4W9ZZAPS5lgAt4JAsxW/nLrnmJWfCu5Vh19nerq/nooyzzsDL0SMr/DwBq+vGhKPWA+p/yzPINc9DvoRJ4f1qWxY2eb+ujWfFPbqx+6Ra0/Jbjh0zg18fqC/Mlak61+EXFkcUECgQdB04t89/1O/w1cDnyilFU=')  # 用您的 channel access token 替換
 handler = WebhookHandler('3e49258295882026968a5788967a12f1')  # 用您的 channel secret 替換
 
+@app.route("/", methods=["GET"])
+def index():
+    return "Welcome to the LINE bot!"
+
 @app.route("/callback", methods=["POST"])
 def callback():
     signature = request.headers["X-Line-Signature"]
@@ -27,3 +31,6 @@ def callback():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+
+
